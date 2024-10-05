9365cfde1a01ec27317602802fd4a0fc0af651de
joyce wamocho <joywamo@gmail.com>
2024-10-05 09:56:19 +0300

    task listener project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form")
+  
+  form.addEventListener("submit",(e) =>{
+    e.preventDefault()
+    createList(e.target.submit.value)
+  })
+
+  function createList(){
+    const text = document.getElementById("new-task-description")
+    const ul = document.getElementById("tasks")
+    let li = document.createElement("li")
+    let btn = document.createElement("button")
+
+    btn.addEventListener("click",handleDelete)
+    btn.textContent = "X"
+    li.textContent = text.value
+    li.appendChild(btn)
+    ul.appendChild(li)
+    text.value = ""
+  }
+
+  function handleDelete(e){
+    e.target.parentNode.remove()
+  }
+
+
 });
+
